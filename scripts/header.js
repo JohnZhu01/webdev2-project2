58dc8ac11528dfeef6bfbb86ac65b2f72f2cdb10
Sandy <upgjcj865000@gmail.com>
2026-06-16 11:42:31 -0700

    Add mobile header menu

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,5 @@
 export function initHeader() {
+  const bodyDom = document.querySelector('body')
   const languageHeader = document.querySelector('.header-language')
   const languageButton = document.querySelector('#header-language-btn')
   const languageMenu = document.querySelector('#header-language-menu')
@@ -17,5 +18,25 @@ export function initHeader() {
 
     languageButton.setAttribute('aria-expanded', 'false')
     languageMenu.classList.remove('is-open')
+  })
+
+  const menuMenuHeader = document.querySelector('.header-menu-menu')
+  const menuButton = document.querySelector('#header-menu-btn')
+  const menuCloseButton = document.querySelector('#header-menu-close-btn')
+
+  menuButton.addEventListener('click', () => {
+    menuCloseButton.hidden = false
+    menuMenuHeader.hidden = false
+    menuButton.hidden = true
+    languageButton.hidden = true
+    bodyDom.classList.add('is-menu-open')
+  })
+
+  menuCloseButton.addEventListener('click', () => {
+    menuCloseButton.hidden = true
+    menuMenuHeader.hidden = true
+    menuButton.hidden = false
+    languageButton.hidden = false
+    bodyDom.classList.remove('is-menu-open')
   })
 }
