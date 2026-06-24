c52866aa23f968fc16cc21155d31221699dd2892
Sandy <upgjcj865000@gmail.com>
2026-06-24 00:47:38 -0700

    Add clients carousel and update html structure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,11 @@
 // Header Start
 import { initHeader } from './header.js'
-
 initHeader()
 // Header End
 
 // Our Clients Start
+import { initOurClients } from './ourClients.js'
+initOurClients()
 // Our Clients End
 
 // Client Testimonials Start
